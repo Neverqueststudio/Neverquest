68550244cea1a30265cacfc34dbf7a3f2d185fb4
unknown <anuspoopswap@gmail.com>
2023-11-27 20:27:35 -0600

    V0.1

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -9,6 +9,10 @@ AAuraCharacterBase::AAuraCharacterBase()
  	
 	PrimaryActorTick.bCanEverTick = true;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon ->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
 }
 
 void AAuraCharacterBase::BeginPlay()
